a9ecbd878de8442acf8cd4bb83c795cbe7b00803
pengcheng <peng.cheng@acyclic.ai>
2026-03-04 13:42:58 -0500

    pre!

--- Lp2lc/Active/F/Def.lean
+++ Lp2lc/Active/F/Def.lean
@@ -3,18 +3,3 @@ import «Lp2lc».Active.Shared
 import «Lp2lc».Active.STLC
 
 namespace Lp2lc.Active.F
-
-inductive Typ : Type where
-  | stlc : STLC.Typ -> Typ
-  | bvar : Nat -> Typ
-  | fvar : Var -> Typ
-deriving DecidableEq, Repr
-
-inductive Trm : Type where
-  | stlc: STLC.Trm -> Trm
-  | tabs : Trm -> Trm
-  | tapp : Trm -> Typ -> Trm
-deriving DecidableEq, Repr
-
-inductive Bind: Type where
-  | stlc: STLC.Bind -> Bind
