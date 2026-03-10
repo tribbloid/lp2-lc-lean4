886f0b74e102d73fe9d33110939e69ae744b4875
pengcheng <peng.cheng@acyclic.ai>
2026-03-10 02:17:06 -0400

    pre!

--- Lp2lc/Active/STLC.lean
+++ Lp2lc/Active/STLC.lean
@@ -1,4 +1,2 @@
 import «Lp2lc».Active.STLC.Def
 import «Lp2lc».Active.STLC.Proof
-import «Lp2lc».Active.STLCv1.Def
-import «Lp2lc».Active.STLCv1.Proof
---
+++ Lp2lc/Active/STLCv1.lean
@@ -0,0 +1,3 @@
+
+import «Lp2lc».Active.STLCv1.Def
+import «Lp2lc».Active.STLCv1.Proof
