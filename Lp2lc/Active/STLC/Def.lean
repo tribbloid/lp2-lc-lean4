7e40452e847d01e263135c3ceed39e64e02b478a
pengcheng <peng.cheng@acyclic.ai>
2026-03-22 17:16:57 -0400

    leak operator is vulnerable to infinite unfolding and is removed, all operators now use F-bound directly
    need fix

--- Lp2lc/Active/STLC/Def.lean
+++ Lp2lc/Active/STLC/Def.lean
@@ -3,6 +3,10 @@ import «Lp2lc».Active.Shared
 
 namespace Lp2lc.Active
 
+structure BiMap (α β : Type) where
+  fwd : α -> β
+  inv : β -> α
+
 -- System extension, induction rule always cast input into output in either the same system or the most specific system that defined the rule
 -- AKA, Typ and Trm are always closed under rules/composition/induction
 -- e.g. using STLC rule can cast STLC Typ into STLC Typ, or F Typ into F Typ
@@ -13,8 +17,9 @@ namespace Lp2lc.Active
 
 namespace LC -- untyped lambda calculus
 
-inductive Typ (F: Type): Type -- F stands for "fixed-point"
-| leaks: F -> Typ F -- AKA wildcard, others, unknown, can be any type symbol
+inductive Typ
+  (F: Type) --AKA wildcard, others, unknown, can be any type symbol
+  : Type -- F stands for "fixed-point"
 | all : Typ F
 deriving Repr
 
@@ -23,8 +28,8 @@ end LC
 namespace STLC -- simply-typed lambda calculus
 
 inductive Typ (F: Type): Type
-| backbone: LC.Typ F -> Typ F
-| arrow : Typ F -> (Typ F) -> (Typ F)
+| backbone: LC.Typ F -> Typ F -- should cost 0 fuel in induction, but no problem if it cost 1 fuel
+| arrow : F -> F -> (Typ F)
 deriving Repr
 -- together they can write any type expression, e.g.
 
@@ -80,8 +85,7 @@ namespace P1
 
 -- assuuming you have a generic, extendable theorem for STLC Typ:
 theorem stlcProof (F : Type)
-  (mk : STLC.Typ F -> F)
-  (unmk : F -> STLC.Typ F) -- TODO: not sure if ⊕' can be moved to call-site
+  (view : BiMap (STLC.Typ F) F) -- TODO: not sure if ⊕' can be moved to call-site
   : STLC.Typ F -> SomeBullshitConjecture :=
   sorry
 
@@ -93,12 +97,14 @@ theorem sysFCorollary (F : Type)
   | .bvar n => bvarLemma F n
   | .fvar x => fvarLemma F x
   | .backbone t =>
-    let ev1(typ: STLC.Typ F): F := mk (Typ.backbone typ)
-    let ev2(real: F): STLC.Typ F :=
-      let typ := unmk real
+    let view : BiMap (STLC.Typ F) F := {
+      fwd := fun typ => mk (Typ.backbone typ)
+      inv := fun real =>
+        let typ := unmk real
 
-      sorry
-    stlcProof F ev1 ev2 t
+        sorry
+    }
+    stlcProof F view t
     -- let t' := unmk t
     -- stlcProof F unmk t
 
@@ -106,43 +112,66 @@ end P1
 
 namespace P2
 
-
 -- assuuming you have a generic, extendable theorem for STLC Typ:
 theorem stlcProof (F : Type)
-  (mk : STLC.Typ F <-> F)
-  (v: F)
+  (_view : BiMap (STLC.Typ F) F) -- assuming that both to_fun and inv_fun are total and terminating
+  (_v : F)
   : SomeBullshitConjecture :=
   sorry
 
 -- assuuming you have a generic, extendable theorem for STLC Typ:
-theorem stlcProofRelaxed (F : Type)
+theorem stlcProofRelaxed (F : Type) -- THIS should NOT happen.
   (mk : STLC.Typ F -> F)
-  (unmk : F -> (STLC.Typ F ⊕' SomeBullshitConjecture)) -- TODO: not sure if ⊕' can be moved to call-site
+  (unmk : F -> (STLC.Typ F ⊕' SomeBullshitConjecture)) -- contains a shortcut to the conjecture directly
   (v: F)
   : SomeBullshitConjecture :=
-  match unmk v with
-  | .inl t => stlcProof F mk unmk t
-  | .inr p => p
+    match unmk v with
+    | .inl (t: STLC.Typ F) =>
+      stlcProof F {
+        fwd := mk
+        inv := fun real =>
+          match unmk real with
+          | .inl t' => t'
+          | .inr _ => t
+          -- t
+      }
+        (mk t)
+    | .inr p => p
 
 
--- theorem sysFCorollary (F : Type)
---   (mk : Typ F -> F)
---   (unmk : F -> Typ F)
---   (v: F)
---   : SomeBullshitConjecture :=
---     match unmk v with
---     | .bvar n => bvarLemma f n
---     | .fvar x => fvarLemma f n
---     | .backbone t =>
+theorem sysFCorollary (F : Type)
+  (mk : BiMap (Typ F) F)
+  (v: F)
+  : SomeBullshitConjecture :=
+    match mk.inv v with
+    | .bvar n => bvarLemma F n
+    | .fvar x => fvarLemma F x
+    | .backbone (t: STLC.Typ F) =>
+      let view : BiMap (STLC.Typ F) F := {
+        fwd := fun typ => mk.fwd (Typ.backbone typ)
+        inv := fun real =>
+          match mk.inv real with
+          | .backbone t' => t'
+          | .bvar _ => t
+          | .fvar _ => t
+      }
+      stlcProof F view v
 
---     let ev1 (typ: STLC.Typ F): F := mk (Typ.backbone typ)
+theorem sysFCorollary2 (F : Type)
+  (mk : BiMap (Typ F) F)
+  (v: F)
+  : SomeBullshitConjecture :=
+    let view : BiMap (STLC.Typ F) F := {
+        fwd := fun typ => mk.fwd (Typ.backbone typ)
+        inv := fun real =>
+          match mk.inv real with
+          | .backbone t' => t'
+          | .bvar n => bvarLemma F n
+          | .fvar x => fvarLemma F x
+      }
+    stlcProof F view v
 
---     let ev2(real: F): STLC.Typ F :=
---       let typ := unmk real
-
---     let result := fun (v: F) => stlcProof F ev1 ev2 v
---     result
-
+w
 end P2
 
 
