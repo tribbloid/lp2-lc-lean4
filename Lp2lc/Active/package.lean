384d90d9457e1fd5a6fb40cb7afc7abe9c5494a1
Peng Cheng <peng.cheng@acyclic.ai>
2026-03-30 16:32:04 -0400

    fucked up

--- Lp2lc/Active/package.lean
+++ Lp2lc/Active/package.lean
@@ -2,34 +2,4 @@ import «Lp2lc».Active.LC.Def
 
 -- STLC module exports Def and Example
 import «Lp2lc».Active.STLC.Def
-import «Lp2lc».Active.STLC.Example
-
-import «Lp2lc».Active.SysF.Def
-
-import «Lp2lc».Active.Fsub.Def
-import «Lp2lc».Active.Fsub.Auxiliary
-import «Lp2lc».Active.Fsub.Proof
-
-import «Lp2lc».Active.FsubL_alt.Def
-import «Lp2lc».Active.FsubL_alt.Auxiliary
-import «Lp2lc».Active.FsubL_alt.Proof
-
-import «Lp2lc».Active.Dsub.Def
-import «Lp2lc».Active.Dsub.Auxiliary
-import «Lp2lc».Active.Dsub.Proof
-
-import «Lp2lc».Active.Dsubsup.Def
-import «Lp2lc».Active.Dsubsup.Auxiliary
-import «Lp2lc».Active.Dsubsup.Proof
-
-import «Lp2lc».Active.Ddia.Def
-import «Lp2lc».Active.Ddia.Auxiliary
-import «Lp2lc».Active.Ddia.Proof
-
-import «Lp2lc».Active.Dot.Def
-import «Lp2lc».Active.Dot.Auxiliary
-import «Lp2lc».Active.Dot.Proof
-
-import «Lp2lc».Active.Dot_top_bot.Def
-import «Lp2lc».Active.Dot_top_bot.Auxiliary
-import «Lp2lc».Active.Dot_top_bot.Proof
+import «Lp2lc».Active.STLC.Proof
