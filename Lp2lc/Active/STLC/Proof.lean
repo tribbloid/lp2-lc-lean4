0e33529e6f212715f2e2dc1846483e4970a72106
Peng Cheng <peng.cheng@acyclic.ai>
2026-04-08 20:59:45 -0400

    rename

--- Lp2lc/Active/STLC/Proof.lean
+++ Lp2lc/Active/STLC/Proof.lean
@@ -14,18 +14,29 @@ All docstrings use short (under 5 lines) of Scala code as demonstrations.
 
 namespace Lp2lc.Active.STLC
 
+section
+
+variable {TermVar : Type} [DecidableEq TermVar]
+
 /--
 Step-indexed logical relation describing semantically well-behaved values.
 -/
-def Semantics (type : Ty) (data : type.Denotation) (fuel : Nat) : Prop :=
+def Semantics (type : Ty) (ir : type.Denotation) (fuel : Nat) : Prop :=
   match type with
   | .base => True
-  | .arrow input output =>
-      let fn := data
+  | .arrow tIn tOut =>
+      let fn := ir
       ∀ lessFuel, lessFuel ≤ fuel →
-        ∀ (vIn : input.Denotation), Semantics input vIn lessFuel →
-          Later (Semantics output (fn vIn) lessFuel)
+        ∀ (vIn : tIn.Denotation), Semantics tIn vIn lessFuel →
+          Later (Semantics tOut (fn vIn) lessFuel)
 
+/--
+proof of monotonicity so the proposition can be moved forward on time axis
+
+all monotonicity proof are pure Slop and should be reduced to 1 line using iris-lean or sledgehammer
+
+but they are unstable right now
+-/
 lemma Semantics.monotone {fuel lessFuel : Nat} (lessEv : lessFuel ≤ fuel)
     {value : type.Denotation} :
     Semantics type value fuel → Semantics type value lessFuel := by
@@ -47,20 +58,18 @@ lemma Semantics.monotone {fuel lessFuel : Nat} (lessEv : lessFuel ≤ fuel)
       intro testFuel testBound testValue testSemantics
       exact functionSemantics' testFuel (Nat.le_trans testBound lessEv) testValue testSemantics
 
-def REPLSemantics {TermVar : Type} [DecidableEq TermVar]
-    (repl : REPL TermVar) (fuel : Nat) : Prop :=
+
+def REPLSemantics (repl : REPL TermVar) (fuel : Nat) : Prop :=
   ∀ (name : TermVar) (type : Ty) (binding : repl.env.get name = some type),
     Semantics type (repl.varLookup name type binding) fuel
 
-lemma REPLSemantics.monotone {TermVar : Type} [DecidableEq TermVar]
-    {repl : REPL TermVar} {fuel lessFuel : Nat} (lessEv : lessFuel ≤ fuel) :
+lemma REPLSemantics.monotone {repl : REPL TermVar} {fuel lessFuel : Nat} (lessEv : lessFuel ≤ fuel) :
     REPLSemantics repl fuel → REPLSemantics repl lessFuel := by
   intro replSemantics name type binding
   exact Semantics.monotone lessEv (replSemantics name type binding)
 
-lemma REPLSemantics.extend {TermVar : Type} [DecidableEq TermVar]
-    {repl : REPL TermVar} {fuel : Nat} {name : TermVar} {value : input.Denotation}
-    (oldReplSemantics : REPLSemantics repl fuel) (extra : Semantics input value fuel) :
+lemma REPLSemantics.extend {repl : REPL TermVar} {fuel : Nat} {name : TermVar} {value : tIn.Denotation}
+    (oldReplSemantics : REPLSemantics repl fuel) (extra : Semantics tIn value fuel) :
     REPLSemantics (repl.extend name value) fuel := by
   intro testedName type binding
   by_cases sameName : testedName = name
@@ -73,16 +82,19 @@ lemma REPLSemantics.extend {TermVar : Type} [DecidableEq TermVar]
     simpa [REPL.extend, sameName] using
       oldReplSemantics testedName type tailBinding
 
-theorem fundamental {TermVar : Type} [DecidableEq TermVar] [Inhabited TermVar]
-    (repl : REPL TermVar) (newTerm : Env.ScopedTerm repl.env type) :
+section
+
+variable [Inhabited TermVar]
+
+theorem fundamental (repl : REPL TermVar) (newTerm : Env.ScopedTerm repl.env type) :
     ∀ {fuel : Nat},
       REPLSemantics repl fuel →
       Semantics type (repl.eval newTerm) fuel := by
-  let rec go {type : Ty} {term : Tm} (repl : REPL TermVar)
-      (checked : Env.Checked repl.env term type) :
+  let rec go {type : Ty} {tOut : Tm} (repl : REPL TermVar)
+      (checked : Env.Checked repl.env tOut type) :
       ∀ {fuel : Nat},
         REPLSemantics repl fuel →
-        Semantics type (repl.eval ⟨term, checked⟩) fuel := by
+        Semantics type (repl.eval ⟨tOut, checked⟩) fuel := by
     match checked with
     | .var binding =>
         intro fuel replSemantics
@@ -90,34 +102,34 @@ theorem fundamental {TermVar : Type} [DecidableEq TermVar] [Inhabited TermVar]
     | .literal normalForm =>
         intro fuel replSemantics
         simp [Semantics]
-    | @Env.Checked.function _ _ _ body input output bodyChecked =>
+    | @Env.Checked.function _ _ _ body tIn tOut bodyChecked =>
         intro fuel replSemantics
         change
           ∀ lessFuel, lessFuel ≤ fuel →
-            ∀ value, Semantics input value lessFuel →
+            ∀ value, Semantics tIn value lessFuel →
               Later
-                (Semantics output
+                (Semantics tOut
                   ((repl.eval ⟨Tm.function body, Env.Checked.function bodyChecked⟩) value) lessFuel)
         intro lessFuel smallerBound value valueSemantics
         refine ⟨?_⟩
         simpa [REPL.eval] using
-          go (repl.extend (input := input) default value) (bodyChecked default)
+          go (repl.extend (input := tIn) default value) (bodyChecked default)
             (fuel := lessFuel)
             ((REPLSemantics.monotone smallerBound replSemantics).extend valueSemantics)
-    | @Env.Checked.apply _ _ _ function argument input output functionChecked argumentChecked =>
+    | @Env.Checked.apply _ _ _ function argument tIn tOut functionChecked argumentChecked =>
         intro fuel replSemantics
         have functionSemantics :
             ∀ lessFuel, lessFuel ≤ fuel →
-              ∀ value, Semantics input value lessFuel →
+              ∀ value, Semantics tIn value lessFuel →
                 Later
-                  (Semantics output
+                  (Semantics tOut
                     ((repl.eval ⟨function, functionChecked⟩) value) lessFuel) := by
           simpa [Semantics] using
             go repl functionChecked
               (fuel := fuel)
               replSemantics
         have argumentSemantics :
-            Semantics input (repl.eval ⟨argument, argumentChecked⟩) fuel :=
+            Semantics tIn (repl.eval ⟨argument, argumentChecked⟩) fuel :=
           go repl argumentChecked
             (fuel := fuel)
             replSemantics
@@ -129,11 +141,10 @@ theorem fundamental {TermVar : Type} [DecidableEq TermVar] [Inhabited TermVar]
       intro fuel replSemantics
       simpa using go repl checked (fuel := fuel) replSemantics
 
-abbrev closed (TermVar : Type) [DecidableEq TermVar] (type : Ty) :=
+abbrev closed (type : Ty) :=
   Env.ScopedTerm (env := (Env.empty : Env TermVar)) type
 
-theorem soundness {TermVar : Type} [DecidableEq TermVar] [Inhabited TermVar]
-    (term : closed TermVar type) :
+theorem soundness (term : closed type) :
     ∀ fuel, ∃ value,
       (REPL.empty : REPL TermVar).RunsTo term value ∧
       Semantics type value fuel := by
@@ -143,5 +154,9 @@ theorem soundness {TermVar : Type} [DecidableEq TermVar] [Inhabited TermVar]
   · exact fundamental (REPL.empty : REPL TermVar) term (fuel := fuel) (by
       intro _name _innerType binding
       simp at binding)
+
+end
+
+end
 
 end STLC
---
+++ Lp2lc/Active/TODO.md
@@ -0,0 +1,6 @@
+- Ty.Denotation should be gone, term in PHOAS is already describing denotational syntax
+- Semantics should have better names and definition to handle extrinsic typing
+- merge Ty and Tm into inductive AST
+- make it parametrically extendable
+- extend to System F and FSub
+
