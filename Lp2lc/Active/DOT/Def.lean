d34310b78547263156851d1a1843ddbe85d6cd07
Peng Cheng <peng.cheng@acyclic.ai>
2026-04-20 14:09:13 -0400

    wip!

--- Lp2lc/Active/DOT/Def.lean
+++ Lp2lc/Active/DOT/Def.lean
@@ -23,49 +23,49 @@ variable (I: Type)[DecidableEq I] -- index
 
 mutual
 
-inductive Entry: Type where -- member of an object/record
+inductive Entry: Type where -- member of an object/record, visible in both "Trm" and "Typ"
 | typeAlias (tLabel: Label) : Entry -- `{type Label}`, it deliberately contain no type assigment or bound, they are evidence-only terms in the same object ("subtypeEv" only in DOT but will include "co/contravarianceEv" later).
 | term (label: Label) (tm: Trm) : Entry -- `{term label = Tm}`
 
 inductive Typ : Type where
 -- | later (raw: Typ) : Typ -- don't know how to use it in iris yet.
 | primitive : Typ -- `AnyVal`, won't differentiate Int/Float/Byte.
-| depFn (tIn : Typ) (tOut: (arg: I) -> Typ) : Typ -- function `In => Out` or dependent function (if "tOut" uses "arg")
+-- TODO: technically it is a non-depenent function, is this case necessary?
 | subtypeEv (tUnder: Typ) (tOver: Typ) : Typ -- subtype evidence, AKA coercion, `Under <:< Over`
+| depFn (tIn : Typ) (tOut: (arg: I) -> Typ) : Typ -- function `In => Out` or dependent function (if "tOut" uses "arg")
 | entry (single : Entry) : Typ -- AKA object1, record1, 1 member only
 | depSelectTyp (base: Trm) (tK: Label) : Typ -- `base.K`
-| selfBinder (body: (arg: I) -> Typ) : Typ -- AKA Mu-type, the delimiter/wrapper in an AST of which "Trm.self" refers to
+-- TODO: this "body" definition assumes polymorphic output schema depending on input.
+| selfBinder (body: (this: I) -> Typ) : Typ -- AKA Mu-type, body can refer to `this` (If de Bruijn serial is used instead of PHOAS, `this` will have serial "0")
 | singleton (v: Trm) : Typ -- path singleton type that can only bind `v`, `v.type`
 | and (tX: Typ) (tY: Typ) : Typ -- AKA intersection, `X & Y`
 | or (tX: Typ) (tY: Typ) : Typ -- AKA union, `X | Y`
 | top  : Typ -- `Any`
 | bottom  : Typ -- `Nothing`
+-- below are for generics, not part of DOT
+-- | genericApply (ctor: TypCtor) (arg: TypCtor): Typ
 
 inductive Val : Type where -- evaluation results and args of Atomic Normal Form (ANF), `Typ` CANNOT be carried! they are erased at runtime!
 | primitive (repr: ByteCode) : Val -- `3`, `3.2`, `true` etc.
-| object (lookup : ObjectBody Entry) : Val -- carrying a member lookup, DOT only uses structural typing so Trait has to carry an extra hidden type member
-| depFn (body : (arg: I) -> Trm) : Val -- see "Typ.depFn", does it really need a body?
 | subtypeEv : Val -- see "Typ.subtypeEv", has no body, erased at runtime
+-- TODO: does it really need a body? Why can't it be merged into subtypeEv ?
+| depFn (body : (arg: I) -> Trm) : Val -- see "Typ.depFn"
+| object (body : (this: I) -> ObjectBody Entry) : Val -- object/record with a member lookup that can refer to `this`, DOT only uses structural typing so Trait has to carry an extra hidden type member
 
 inductive Trm : Type where -- AKA expression, unlike Val, it is indexed by type
 | var (symbol: I) : Trm -- variable, `x`, always bounded, almost always locally closed (In PHOAS it is imposible to construct wildcard "(symbol: I)")
 | val (v : Val) : Trm -- value, AKA literal
-| self : Trm -- self-var, `this`, must be inside a "Typ.selfBinder". If de Bruijn serial is used for bounded var then this is the "0".
 | depSelectTrm (base: Trm) (label: Label) : Trm  -- `object.label`
 | depApply (fn: Trm) (arg: Trm) : Trm -- application of (dependent?) function
 
--- TODO: some of these can be merged actually, e.g. subtypeEv and depFn
+inductive TypCtor : Type where -- type constructor! not type! not in core DOT!
+| tVar (symbol: I): TypCtor
+| ctor (body: ((arg : I) -> TypCtor)): TypCtor
+-- | higherCtor (body: (arg: I) -> TypCtor) : TypCtor
+| apply (ctor: TypCtor) (arg: TypCtor): TypCtor
 
 end
 
-namespace postpone -- DOT with type constructors/generics with type bound & variance, fully defined Scala.
--- TODO: eventually this part has to be added with examples, including
--- elaborator rules
-
-inductive TypCtor : Type where -- type constructor! not type! not in core DOT!
-| ctor: (arg : I -> Typ I) -> TypCtor
-
-end postpone
 
 /-
 Semantics
