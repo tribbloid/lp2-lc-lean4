0f2f738505ba579e70a9ccc60a43b9f0bb06fae1
Peng Cheng <peng.cheng@acyclic.ai>
2026-04-24 14:55:18 -0400

    wip!

---
+++ Tests/DTLC/Sanity.lean
@@ -0,0 +1,111 @@
+import «Lp2lc».Active.DTLC.Def
+
+
+
+/-
+this is a supporting sanity test file for DOT calculus syntax definition.
+
+each of the AST below are supposed to represent scala variable of the same name
+in @SanityExample.scala
+-/
+
+namespace Tests.DTLC.Sanity
+open Lp2lc.Active.DTLC
+
+namespace Trm
+
+def false : TrmClosed :=
+  .val (.primitive "false")
+
+def true : TrmClosed :=
+  .val (.primitive "true")
+
+def identityFn : TrmClosed :=
+  .val
+    (.depFn (fun x => .var x .primitive))
+
+def identityFnOnFalse : TrmClosed :=
+  .depApply identityFn false
+
+def get1st : TrmClosed :=
+  .val
+    (.depFn (fun x =>
+      .val
+        (.depFn (fun _y => .var x .primitive))))
+
+def get2nd : TrmClosed :=
+  .val
+    (.depFn (fun _x =>
+      .val
+        (.depFn (fun y => .var y .primitive))))
+
+def get1stOnTuple : TrmClosed :=
+  .depApply
+    (.depApply get1st false)
+    true
+
+def get2ndOnTuple : TrmClosed :=
+  .depApply
+    (.depApply get2nd false)
+    true
+
+def apply1stOn2ndFn : TrmClosed :=
+  .val (.depFn (fun f =>
+    .val (.depFn (fun x =>
+      .depApply
+        (.var f (.depFn .primitive (fun _ => .primitive)))
+        (.var x .primitive)))))
+
+def apply1stOn2ndFnOnTuple : TrmClosed :=
+  .depApply
+    (.depApply apply1stOn2ndFn identityFn)
+    false
+
+end Trm
+
+namespace Typ
+
+def false : TypClosed :=
+  .primitive
+
+def true : TypClosed :=
+  .primitive
+
+def identityFn : TypClosed :=
+  .depFn .primitive (fun _x => .primitive)
+
+def identityFnOnFalse : TypClosed :=
+  .primitive
+
+def get1st : TypClosed :=
+  .depFn .primitive (fun _x =>
+    .depFn .primitive (fun _y => .primitive))
+
+def get2nd : TypClosed :=
+  .depFn .primitive (fun _x =>
+    .depFn .primitive (fun _y => .primitive))
+
+def get1stOnTuple : TypClosed :=
+  .primitive
+
+def get2ndOnTuple : TypClosed :=
+  .primitive
+
+def apply1stOn2ndFn : TypClosed :=
+  .depFn
+    (.depFn .primitive (fun _x => .primitive))
+    (fun _f =>
+      .depFn .primitive (fun _x => .primitive))
+
+def apply1stOn2ndFnOnTuple : TypClosed :=
+  .primitive
+
+end Typ
+
+namespace Typing
+end Typing
+
+namespace Eval
+end Eval
+
+end Sanity
