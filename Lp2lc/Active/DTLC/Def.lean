8b32bb3db9998b77e398de7834163fed973bc108
Peng Cheng <peng.cheng@acyclic.ai>
2026-04-25 13:55:28 -0400

    wip!

--- Lp2lc/Active/DTLC/Def.lean
+++ Lp2lc/Active/DTLC/Def.lean
@@ -24,7 +24,7 @@ inductive Typ : Type (u + 1) where
 | depFn (tIn : Typ) (tOut : (arg : I) -> Typ) : Typ
 
 inductive Trm : Type (u + 1) where
-| var (symbol : I) (tAnnotation : Typ) : Trm
+| var (symbol : I) {tAnnotation : Typ} : Trm
 | val (v : Val) : Trm
 | depApply (fn : Trm) (arg : Trm) : Trm
 
@@ -35,66 +35,24 @@ end
 
 end Syntax
 
-abbrev TrmClosed := {I : Sort 1} -> Trm I
+abbrev TrmClosed := {I : Sort u} -> Trm I
 
-abbrev TypClosed := {I : Sort 1} -> Typ I
+abbrev TypClosed := {I : Sort u} -> Typ I
 
-abbrev ByteCodeTrm := Trm ByteCode
+structure RuntimeCarrier where
+  self: ByteCode
 
-abbrev ByteCodeVal := Val ByteCode
-
-section Semantics
-
-@[simp]
-private def evalBytecode (fuel : Nat) (trm : ByteCodeTrm) : Option ByteCodeVal :=
-  match fuel with
-  | 0 => none
-  | fuel + 1 =>
-      match trm with
-      | Trm.var symbol _ => some (Val.primitive symbol)
-      | Trm.val (Val.primitive repr) => some (Val.primitive repr)
-      | Trm.val (Val.depFn body) => some (Val.depFn body)
-      | Trm.depApply fn arg =>
-          match evalBytecode fuel fn, evalBytecode fuel arg with
-          | some (Val.depFn body), some (Val.primitive argByte) =>
-              evalBytecode fuel (body argByte)
-          | _, _ => none
-
-@[simp]
-private def typingBytecode (fuel : Nat) (trm : ByteCodeTrm) (typ : Typ ByteCode) : Prop :=
-  match fuel with
-  | 0 => False
-  | fuel + 1 =>
-      match trm with
-      | Trm.var _ tAnnotation => tAnnotation = typ
-      | Trm.val (Val.primitive _) => typ = Typ.primitive
-      | Trm.val (Val.depFn body) =>
-          match typ with
-          | Typ.primitive => False
-          | Typ.depFn tIn tOut =>
-              ∀ arg : ByteCode,
-                typingBytecode fuel (Trm.val (Val.primitive arg)) tIn ->
-                typingBytecode fuel (body arg) (tOut arg)
-      | Trm.depApply fn arg =>
-          ∃ tIn tOut argByte,
-            typingBytecode fuel fn (Typ.depFn tIn tOut) ∧
-            typingBytecode fuel arg tIn ∧
-            evalBytecode fuel arg = some (Val.primitive argByte) ∧
-            typ = tOut argByte
+abbrev RuntimeVal := Val RuntimeCarrier
 
 namespace TrmClosed
 
 @[simp]
-def eval (trm : TrmClosed) (fuel : Nat) : Option ByteCodeVal :=
-  evalBytecode fuel (trm (I := ByteCode))
+def eval (trm : TrmClosed) (fuel : Nat) : Option RuntimeVal := sorry
 
-@[simp]
-def typing (trm : TrmClosed) (fuel : Nat) (typ : TypClosed) : Prop :=
-  typingBytecode fuel (trm (I := ByteCode)) (typ (I := ByteCode))
+-- @[simp]
+-- def typing (trm : TrmClosed) (fuel : Nat) (typ : TypClosed) : Prop := sorry
 
 end TrmClosed
-
-end Semantics
 
 end DTLC
 
--- Tests/DTLC/Sanity.lean
+++ Tests/DTLC/Sanity.lean
@@ -22,7 +22,7 @@ def true : TrmClosed :=
 
 def identityFn : TrmClosed :=
   .val
-    (.depFn (fun x => .var x .primitive))
+    (.depFn (fun x => .var (tAnnotation := Typ.primitive) x))
 
 def identityFnOnFalse : TrmClosed :=
   .depApply identityFn false
@@ -31,13 +31,13 @@ def get1st : TrmClosed :=
   .val
     (.depFn (fun x =>
       .val
-        (.depFn (fun _y => .var x .primitive))))
+        (.depFn (fun _y => .var (tAnnotation := Typ.primitive) x))))
 
 def get2nd : TrmClosed :=
   .val
     (.depFn (fun _x =>
       .val
-        (.depFn (fun y => .var y .primitive))))
+        (.depFn (fun y => .var (tAnnotation := Typ.primitive) y))))
 
 def get1stOnTuple : TrmClosed :=
   .depApply
@@ -53,8 +53,10 @@ def apply1stOn2ndFn : TrmClosed :=
   .val (.depFn (fun f =>
     .val (.depFn (fun x =>
       .depApply
-        (.var f (.depFn .primitive (fun _ => .primitive)))
-        (.var x .primitive)))))
+        (.var
+          (tAnnotation := Typ.depFn Typ.primitive (fun _ => Typ.primitive))
+          f)
+        (.var (tAnnotation := Typ.primitive) x)))))
 
 def apply1stOn2ndFnOnTuple : TrmClosed :=
   .depApply
@@ -102,47 +104,49 @@ def apply1stOn2ndFnOnTuple : TypClosed :=
 
 end Typ
 
-namespace Typing
-
-example : TrmClosed.typing Trm.false 2 Typ.false := by
-  simp [TrmClosed.typing, Trm.false, Typ.false]
-
-example : TrmClosed.typing Trm.true 2 Typ.true := by
-  simp [TrmClosed.typing, Trm.true, Typ.true]
-
-example : TrmClosed.typing Trm.identityFn 3 Typ.identityFn := by
-  simp [TrmClosed.typing, Trm.identityFn, Typ.identityFn]
-
-example : TrmClosed.typing Trm.get1st 4 Typ.get1st := by
-  simp [TrmClosed.typing, Trm.get1st, Typ.get1st]
-
-example : TrmClosed.typing Trm.get2nd 4 Typ.get2nd := by
-  simp [TrmClosed.typing, Trm.get2nd, Typ.get2nd]
-
-example : ¬ TrmClosed.typing Trm.apply1stOn2ndFnOnTuple 6 Typ.apply1stOn2ndFnOnTuple := by
-  simp [TrmClosed.typing, Trm.apply1stOn2ndFnOnTuple, Trm.apply1stOn2ndFn, Trm.identityFn,
-    Trm.false,
-    Typ.apply1stOn2ndFnOnTuple]
-
-end Typing
 
 namespace Eval
 
-example : TrmClosed.eval Trm.identityFnOnFalse 1 = some (.primitive "false") := by
-  rfl
+example : TrmClosed.eval Trm.identityFnOnFalse 1 = some (Val.primitive "false") := by
+  sorry
 
-example : TrmClosed.eval Trm.identityFnOnFalse 3 = some ((.primitive "false") : ByteCodeVal) := by
-  rfl
+example : TrmClosed.eval Trm.identityFnOnFalse 3 = some ((Val.primitive "false") : ByteCodeVal) := by
+  sorry
 
-example : TrmClosed.eval Trm.get1stOnTuple 5 = some ((.primitive "false") : ByteCodeVal) := by
-  rfl
+example : TrmClosed.eval Trm.get1stOnTuple 5 = some ((Val.primitive "false") : ByteCodeVal) := by
+  sorry
 
-example : TrmClosed.eval Trm.get2ndOnTuple 5 = some ((.primitive "true") : ByteCodeVal) := by
-  rfl
+example : TrmClosed.eval Trm.get2ndOnTuple 5 = some ((Val.primitive "true") : ByteCodeVal) := by
+  sorry
 
-example : TrmClosed.eval Trm.apply1stOn2ndFnOnTuple 6 = some (.primitive "false") := by
-  rfl
+example : TrmClosed.eval Trm.apply1stOn2ndFnOnTuple 6 = some (Val.primitive "false") := by
+  sorry
 
 end Eval
+
+-- namespace Typing
+
+-- example : TrmClosed.typing Trm.false 2 Typ.false := by
+--   simp [TrmClosed.typing, Trm.false, Typ.false]
+
+-- example : TrmClosed.typing Trm.true 2 Typ.true := by
+--   simp [TrmClosed.typing, Trm.true, Typ.true]
+
+-- example : TrmClosed.typing Trm.identityFn 3 Typ.identityFn := by
+--   simp [TrmClosed.typing, Trm.identityFn, Typ.identityFn]
+
+-- example : TrmClosed.typing Trm.get1st 4 Typ.get1st := by
+--   simp [TrmClosed.typing, Trm.get1st, Typ.get1st]
+
+-- example : TrmClosed.typing Trm.get2nd 4 Typ.get2nd := by
+--   simp [TrmClosed.typing, Trm.get2nd, Typ.get2nd]
+
+-- example : ¬ TrmClosed.typing Trm.apply1stOn2ndFnOnTuple 6 Typ.apply1stOn2ndFnOnTuple := by
+--   simp [TrmClosed.typing, Trm.apply1stOn2ndFnOnTuple, Trm.apply1stOn2ndFn, Trm.identityFn,
+--     Trm.false,
+--     Typ.apply1stOn2ndFnOnTuple]
+
+-- end Typing
+
 
 end Sanity
