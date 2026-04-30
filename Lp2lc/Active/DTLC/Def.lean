af9198702547d3fd64b9ddfe08aaeb82100d3ced
pengcheng <peng.cheng@acyclic.ai>
2026-04-30 18:27:30 -0400

    wip!

--- Lp2lc/Active/DTLC/Def.lean
+++ Lp2lc/Active/DTLC/Def.lean
@@ -9,9 +9,24 @@ namespace DTLC
 dependently typed lambda calculus (similar to STLC but function output type can depend on input term) with a top/wildcard type.
 -/
 
+universe u v
+
+-- 1. Implicitly lift a type to a higher universe using ULift
+instance : Coe (Type u) (Type (max u v)) where
+  coe := ULift
+
+-- 2. Implicitly lift the values of that type into the ULift wrapper, these 2 enabled universe cumulativity in rocq
+instance {α : Type u} : Coe α (ULift.{v, u} α) where
+  coe := ULift.up
+
+structure K : Type
+
+abbrev K1: Type 1 := K
+
 open Util
 
 def Index := Type
+
 
 section Syntax
 
