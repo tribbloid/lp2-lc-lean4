3a7b3d5ebb40418733a5de2c0e8b4f937254f8bd
pengcheng <peng.cheng@acyclic.ai>
2026-05-22 16:01:03 -0400

    squash!

--- Lp2lc/Experiments/DavidExample.lean
+++ Lp2lc/Experiments/DavidExample.lean
@@ -1,16 +1,16 @@
 
+set_option synthInstance.checkSynthOrder false
+
 
 inductive Vec : Nat -> Type
 | mk (n: Nat) (data: List Nat) : Vec n
 deriving DecidableEq
 
-def SameVecType : Prop := ∀ (x y : Nat) (_: x = y), (Vec x) = (Vec y)
-
-def sameVecType : SameVecType := sorry
-
-def vecTransport (x y : Nat) (eq: x = y) : Coe (Vec x) (Vec y) := sorry
+instance vecTransport {x y : Nat} {ev: x = y} : Coe (Vec x) (Vec y) where
+  coe v := cast (congrArg Vec ev) v
 
 def testVecTransport (a b : Nat) : Unit :=
-  let v1 : Vec (a + b) := Vec.mk (a + b) List.empty
-  let v2 : Vec (b + a) := v1
+  let v1 : Vec (a + b) := Vec.mk (a + b) []
+  let _v2 : Vec (b + a) := v1
+  let _v2Hell : Vec (b + a) := (vecTransport (ev := Nat.add_comm a b)).coe v1
   .unit
