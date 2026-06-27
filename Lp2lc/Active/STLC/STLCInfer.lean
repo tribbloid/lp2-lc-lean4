a4f09872feb19eddb2a97e286591fca431d85a0a
Peng Cheng <peng.cheng@acyclic.ai>
2026-06-27 18:32:27 -0400

    cleanup

--- Lp2lc/Active/STLC/STLCInfer.lean
+++ Lp2lc/Active/STLC/STLCInfer.lean
@@ -62,39 +62,6 @@ def proof : @Safety I env := sorry
 
 end Safety
 
--- /--
--- trm with a built-in safety proof
-
--- it's the evaluation target of compile function, like value to eval function
--- -/
--- structure SafeTrm [CompilerEnv I] (I : Free) where
---   trm : AST.Trm I
---   typ : AST.Typ I
---   proof: [RuntimeEnv] -> (trm.infer.shouldYields typ) /\   Rec trm.infer.shouldYields typ -- in STLC this is just an equality! extension will happen later
-
--- -- TODO: mimic Trm.recInfer
--- def proveSafety [fb: FBound I.Index (SafeTrm I)] (trm: AST.Trm I): RecOption (SafeTrm I)
--- | 0 => .outOfFuel
--- | _fule + 1 =>
---   match trm with
---   | .ref k =>
---     -- TODO get AdequateTrm I directly from Compiler Env
---     let v := fb.load (p := ()) k
---     Outcome.yield v
---   | .apply left right => -- TODO: application
---     let _ll := proveSafety left
---     let _rr := proveSafety right
---     ll.typ
---     sorry
---   | .val v =>
---     let condition : Condition I := fun _ => True
---     let proof : trm.CanSatisfy_semi condition := sorry -- TODO trivial, introduce 1 fuel, run Trm.eval immediately and proof CanSatisfy.
---     .mk trm condition proof
-
---     -- argue that _ll must be a function, _rr must be a value of compatible type
-
---     sorry
-
 end
 end STLC
 
