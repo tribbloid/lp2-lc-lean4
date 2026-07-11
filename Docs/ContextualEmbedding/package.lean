671187a9e0e86674de395668e37f3512f3d902af
Peng Cheng <peng.cheng@acyclic.ai>
2026-07-11 16:15:24 -0400

    Split part of STLCCtx into Val

--- Docs/ContextualEmbedding/package.lean
+++ Docs/ContextualEmbedding/package.lean
@@ -1,5 +1,6 @@
 import «ContextualEmbedding».CE
 import «ContextualEmbedding».__CEFnExample
 import «ContextualEmbedding».«1_CleanUpIndex».CE
+import «ContextualEmbedding».«2_SplitVal».CE
 import «ContextualEmbedding».CELeveledUpSTLC
 import «ContextualEmbedding».CELeveledUpLinear
