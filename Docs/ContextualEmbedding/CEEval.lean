bf182260cd57dd149719a7d604ad4a8299df6dde
Peng Cheng <peng.cheng@acyclic.ai>
2026-07-13 16:31:40 -0400

    squash!

--- Docs/ContextualEmbedding/CEEval.lean
+++ Docs/ContextualEmbedding/CEEval.lean
@@ -20,6 +20,15 @@ end STLC
 
 namespace STLCCtx
 
+inductive Val : Ctx -> Ty -> Type where
+  | CStar {ctx : Ctx} : Val ctx Unit
+  | CLam {ctx : Ctx} {input output : Ty}
+      (body : ProxyTop (ctx :/: input) input ->
+        STLCCtx (ctx :/: input) output) :
+      Val ctx (input :-> output)
+
+/-- Contextually embedded values with no free variables. -/
+abbrev ClosedVal (ty : Ty) : Type := Val Ctx.Empty ty
 
 
 end STLCCtx
