50944441632aa7c603006134f7dbee254f733712
Peng Cheng <peng.cheng@acyclic.ai>
2026-07-13 16:27:57 -0400

    scaffold for CEEval

--- Docs/ContextualEmbedding/CEEval.lean
+++ Docs/ContextualEmbedding/CEEval.lean
@@ -4,6 +4,8 @@ namespace ContextualEmbedding.CE
 
 open Ty
 
+namespace STLC
+
 /-- STLC value syntax for unit and function values in a configurable context. -/
 inductive Val : Ctx -> Ty -> Type where
   | Star {ctx : Ctx} : Val ctx Unit
@@ -13,5 +15,13 @@ deriving Repr, DecidableEq
 
 /-- STLC values with no free variables. -/
 abbrev ClosedVal (ty : Ty) : Type := Val Ctx.Empty ty
+
+end STLC
+
+namespace STLCCtx
+
+
+
+end STLCCtx
 
 end ContextualEmbedding.CE
