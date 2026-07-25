1ac0222d624e860ae6cf64575734feadad4c7d32
Peng Cheng <peng.cheng@acyclic.ai>
2026-07-25 14:36:33 -0400

    second draft for umbral __Infer

--- Lp2lc/Active/STLC/__Infer_umbral.lean
+++ Lp2lc/Active/STLC/__Infer_umbral.lean
@@ -13,44 +13,44 @@ namespace Umbral
 
 section variable [@ProvingBase F]
 
-structure InferenceWProof (trm2typ: AST.Trm2Typ F) : Type where
+structure ProvenCondition (trm2typ: AST.Trm2Typ F) : Type where
   sameInfer: trm2typ.trm.infer.isDecidable (λ t2 => trm2typ.typ <= t2)
-  safetyProof: Safety trm2typ.trm trm2typ.typ
+  safety: Safety trm2typ.trm trm2typ.typ -- TODO: should this objective be delayed?
 
 end
 
 /--
-primary key structure to link/associate a CompilerEnv.typeCtx entry and a RuntimeEnv.valueCtx entry
+contains a FBound store to save/load intermediate safety proof for both `AST.Trm` and `AST.Val`
 
-there is no guarantee that typeUID & valueUID will be identical, so this is the only evidence that they are related
+`infer_prove` & any theorem that relies on safety can use it, this is the only correspondence between compiletime and runtime variables.
 -/
--- structure UIDLink where
---   typeUID: F.Index
---   valueUID: F.Index
-
 class ProvingEnv where
   base: @ProvingBase F
-  proofCtx : base.trm2typCtx.Aux (λ t => @InferenceWProof F base t) -- TODO: need a concrete value type to save proof
+  proofCtx : base.trm2typCtx.Aux (λ t => ProvenCondition t)
 
 instance [env: @ProvingEnv F] : @ProvingBase F := env.base
 
 section variable [@ProvingEnv F]
 
 /--
-similar to Trm.infer, even have similar structure
+like `Trm.infer` it inductively infer `Typ` of a given `Trm`, using the structure of `Trm.infer` as a blueprint.
 
-but instead of producing only a Typ F, it is obliged to produce all the following elements:
-- Typ F
-- proof that it is equal to the result of Trm.infer
-- proof that it is safe after evaluation
+unlike `Trm.infer` it is obliged to produce a `ProvenCondition` bundle of:
+
+- original `Typ`
+- proof that it has the same result to `Trm.infer`
+- proof that the `Trm : Typ` pair is safe to evaluate
 -/
-def infer_umbral [env: @ProvingEnv F] (trm : AST.Trm F) :
-    RecOption (PSigma (λ typ : AST.Typ F => @InferenceWProof F env.base ⟨trm, typ⟩)) :=
+def infer_prove [env: @ProvingEnv F] (trm : AST.Trm F) :
+    RecOption (PSigma (λ typ : AST.Typ F => @ProvenCondition F env.base ⟨trm, typ⟩)) :=
   sorry
 
 -- theorem termInferMonotone [env : @ProvingEnv F]
 --     (trm : AST.Trm F) :
 --      (infer trm).Monotone := sorry
+
+
+-- def eval_prove [env: @ProvingEnv F] (trm: AST.Trm F): RecOptionn ()
 
 end
 
