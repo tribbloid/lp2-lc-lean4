c0d131c2049ffcc0dea9fc7b224f5630e1108455
Peng Cheng <peng.cheng@acyclic.ai>
2026-08-05 21:13:28 -0400

    update proof objective & delete old proof

--- Lp2lc/Active/STLC/__Infer_umbral.lean
+++ Lp2lc/Active/STLC/__Infer_umbral.lean
@@ -33,17 +33,14 @@ contains a FBound store to save/load intermediate safety proof for both `AST.Trm
 
 `infer_prove` & any theorem that relies on safety can use it, this is the only correspondence between compiletime and runtime variables.
 -/
-class ProvingEnv where
-  base: @ProvingBase F
+class ProvingEnv extends @ProvingBase F where
+
 
 namespace ProvingEnv
 -- TODO: add proofCtx here, a new Aux0 should be created. No abstract function is allowed
 end ProvingEnv
 
-instance [env: @ProvingEnv F] : @ProvingBase F := env.base
-
 section variable [@ProvingEnv F]
-
 
 /--
 like `Trm.infer` it inductively infer `Typ` of a given `Trm`, using the structure of `Trm.infer` as a blueprint.
@@ -54,48 +51,8 @@ unlike `Trm.infer` it is obliged to produce a `ProvenCondition` bundle of:
 - proof that it has the same result to `Trm.infer`
 - proof that the `Trm : Typ` pair is safe to evaluate
 -/
-def infer_prove [env: @ProvingEnv F] (trm : AST.Trm F) : ProvingResult trm :=
-  λ
-  | 0 => .outOfFuel
-  | fuel + 1 =>
-    match trm with
-    | .val (.lit repr) =>
-      .yield (some ⟨.primitive, by
-        unfold ProvenCondition Safety
-        intro runtimeFuel
-        cases runtimeFuel with
-        | zero => simp [AST.eval]
-        | succ runtimeFuel =>
-          simp [AST.eval, AST.CanInhabit]
-          exact ⟨1, by
-            simp [AST.infer]
-            rfl⟩⟩)
-    | .val (.lam body tIn) =>
-      let index := env.base.trm2typCtx.getUID ⟨.val (.lam body tIn), tIn⟩
-      match (infer_prove (body index)) fuel with
-      | .outOfFuel => .outOfFuel
-      | .yield none => .yield none
-      | .yield (some _) =>
-        match hInfer : (body index).infer fuel with
-        | .outOfFuel => .outOfFuel
-        | .yield none => .yield none
-        | .yield (some tOut) =>
-          .yield (some ⟨.fn tIn tOut, by
-            unfold ProvenCondition Safety
-            intro runtimeFuel
-            cases runtimeFuel with
-            | zero => simp [AST.eval]
-            | succ runtimeFuel =>
-              simp [AST.eval, AST.CanInhabit]
-              refine ⟨fuel + 1, ?_⟩
-              simp [AST.infer, index, hInfer, Rec.Outcome.map]
-              rfl⟩)
-    | .apply fn arg =>
-      match (infer_prove fn) fuel, (infer_prove arg) fuel with
-      | .outOfFuel, _ => .outOfFuel
-      | _, .outOfFuel => .outOfFuel
-      | _, _ => .yield none
-    | @AST.ref _ _ => .yield none
+def infer_prove [env: @ProvingEnv F] (trm : AST.Trm F) : Objective trm :=
+  sorry
 
 -- theorem termInferMonotone [env : @ProvingEnv F]
 --     (trm : AST.Trm F) :
