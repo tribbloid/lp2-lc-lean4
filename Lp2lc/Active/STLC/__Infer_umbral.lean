688b381367c5172b123d53226155a507d3b6161a
Peng Cheng <peng.cheng@acyclic.ai>
2026-08-11 15:31:26 -0400

    wip!

--- Lp2lc/Active/STLC/__Infer_umbral.lean
+++ Lp2lc/Active/STLC/__Infer_umbral.lean
@@ -52,6 +52,16 @@ end ProvingEnv
 
 section
 
+/-
+TODO: **This prove can be shorter!** With the following technique:
+
+- create a new Context [safetyCtx] under [ProvingEnv] that extends existing [trm2typCtx], which is capable of memorizing Term safety or Objective directly
+- temporarily move the AST into a difference PHOAS carrier ([F.Index]) to enable reference to [safetyCtx.Receipt]
+- once the main objective is proven, move back to the original PHOAS carrier
+
+With the new context, it can be made significantly shorter
+-/
+
 /-- Mirrors term inference while routing typed binders through the temporary proof context.
 
 like [AST.infer] it inductively infer [AST.Typ] of a given [AST.Trm], using the structure of [AST.infer] as a blueprint.
