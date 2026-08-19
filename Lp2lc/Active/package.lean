c205c37c365ac37a10179a585c911a530d7adb03
Peng Cheng <peng.cheng@acyclic.ai>
2026-08-19 14:12:03 -0400

    rename

--- Lp2lc/Active/package.lean
+++ Lp2lc/Active/package.lean
@@ -6,7 +6,7 @@ import «Lp2lc».Active.PartialDefInheritance
 -- STLC module exports STLCDef; scratch examples are imported from Tests.
 import «Lp2lc».Active.STLC.STLCDef
 import «Lp2lc».Active.STLC.Proof
-import «Lp2lc».Active.STLC.UmbralV1
+import «Lp2lc».Active.STLC.__Infer_umbralV1
 import «Lp2lc».Active.STLC.__Infer
 import «Lp2lc».Active.STLC.__Infer_umbral
 -- import «Lp2lc».Active.STLC.__Infer_umbral_weaken
