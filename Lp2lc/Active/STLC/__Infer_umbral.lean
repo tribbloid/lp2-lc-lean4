0941e93203e1b353bffe81f9a5c6da3ea17dbc49
Peng Cheng <peng.cheng@acyclic.ai>
2026-08-19 15:04:24 -0400

    TODO only

--- Lp2lc/Active/STLC/__Infer_umbral.lean
+++ Lp2lc/Active/STLC/__Infer_umbral.lean
@@ -18,6 +18,13 @@ structure TypeWithSafey [env : ProvingBase] (trm : AST.Trm env.ExeParameters) wh
   safety : Safety trm t2
 
 
+/-
+TODO: discharge this function.
+
+In general function should have identical structure with Trm.infer in __Infer.lean, but every output is a `TypeWithSafey`, the safety of the term argument have to be proven on-spot
+
+The original trm2valCtx and trm2typCtx are not designed to hold TypeWithSafey, you will need to make some new context for that
+-/
 /-- Infers build types for executable terms. -/
 def infer [env : UmbralEnv]
     (trm : AST.Trm env.ExeParameters) : RecOpt (TypeWithSafey trm) := sorry
