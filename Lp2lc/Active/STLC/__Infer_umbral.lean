6ae0cdf4c58d4c232ce8b6217fa332c71d1b8a16
Peng Cheng <peng.cheng@acyclic.ai>
2026-08-22 15:11:28 -0400

    TODO

--- Lp2lc/Active/STLC/__Infer_umbral.lean
+++ Lp2lc/Active/STLC/__Infer_umbral.lean
@@ -12,23 +12,23 @@ structure TypeWithSafey {core} [build : BuildEnv core]
   safety : [_exe : ExeEnv core] -> Safety trm t2
 
 class ProvingEnv (core : EnvCore) extends BuildEnv core where
-  trm2typWithSafetyCtx :=
+  uid2typWithSafetyCtx :=
     (toBuildEnv.uid2typCtx (trm2typ := uid2typ)).mkLesser
       (λ _v => PSigma (λ (trm : AST.Trm core.ExeParameters) => TypeWithSafey (build := toBuildEnv) trm))
 
 /-
 This is an agumented version of [AST.Trm.infer].
 
-The scaffold
+There is only 1 difference: it must produce a type judge with safety proof that trm always evaluate to a value of the same type
+
+It also has access to [ProvingEnv], a mirror of [BuildEnv] with [uid2typWithSafetyCtx] : an extra equivalence between type with safety proof and a subtype of UId
+
+You are also encouraged to make more context to meet proving demand
 
 TODO: discharge this function.
-
-In general function should have identical structure with Trm.infer in __Infer.lean, but every output is a `TypeWithSafey`, the safety of the term argument have to be proven on-spot
-
-The original trm2valCtx and trm2typCtx are not designed to hold TypeWithSafey, you will need to make some new context for that
 -/
 /-- Infers build types for executable terms. -/
-def infer [core : EnvCore] [build : BuildEnv core] [env : ExeEnv core]
+def infer [core : EnvCore] [proving : ProvingEnv core] [env : ExeEnv core]
     (trm : AST.Trm core.ExeParameters) : RecOpt (TypeWithSafey trm) := sorry
 
 end Umbral
